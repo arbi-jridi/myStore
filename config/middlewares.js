69a9d571a71b3c800c59d6feed5545f476a150ce
arbi1990 <arbi.jridi@gmail.com>
2024-08-02 15:53:07 +0100

    add cloudinary

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,22 +1,25 @@
-module.exports = ({ env }) => [
-  "strapi::errors",
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
   {
-    name: "strapi::security",
+    name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
+        useDefaults: true,
         directives: {
-          "default-src": ["'self'"],
-          "img-src": ["'self'", "data:", "blob:", env("SUPABASE_API_URL")],
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
         },
       },
     },
   },
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::logger",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,18 @@
 module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      config: {
-        provider: "strapi-provider-upload-supabase-4",
-        providerOptions: {
-          apiUrl: env("SUPABASE_API_URL"),
-          apiKey: env("SUPABASE_API_KEY"),
-          bucket: env("SUPABASE_BUCKET"),
-          directory: env("SUPABASE_DIRECTORY"),
-          options: {},
-        },
-        actionOptions: {
-          upload: {},
-          uploadStream: {},
-          delete: {},
-        },
+  // ...
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-    // ...
-  });
+  },
+  // ...
+});
